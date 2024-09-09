cf5d68f29914f9d0fd226136fc2226ae545c3ccc
pushechkin524 <134803783+pushechkin524@users.noreply.github.com>
2024-09-09 20:03:38 +0300

    Добавьте файлы проекта.

---
+++ Calculator.Tests/PasswordTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Password.Tests
+{
+    [TestClass]
+    public class PasswordTest
+    {
+        [TestMethod]
+        public void PasswordValidation_password_0()
+        {
+            var ending = 1;
+            var password = "6524138765";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        }
+
+
+        [TestMethod]
+        public void PasswordValidation_password_1()
+        {
+            var ending = 1;
+            var password = "6524138765";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        }  
+        
+        [TestMethod]
+        public void PasswordValidation_password_2()
+        {
+            var ending = 2;
+            var password = "819743627649785686928374";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        }
+        
+        [TestMethod]
+        public void PasswordValidation_password_3()
+        {
+            var ending = 3;
+            var password = "736483g7836482764";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        } 
+
+
+        [TestMethod]
+        public void PasswordValidation_password_4()
+        {
+            var ending = 4;
+            var password = "DJHSDJSLKJDSLKHJh!";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        } 
+
+
+        [TestMethod]
+        public void PasswordValidation_password_5()
+        {
+            var ending = 5;
+            var password = "6238736248fG!72398";
+            PracticeTestFir.Password pswrd = new PracticeTestFir.Password();
+            var sec = pswrd.PasswordValidation(password);
+            Assert.AreEqual(ending, sec);
+        }
+    }
+}
---
+++ Calculator/Password.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTestFir
+{
+    public class Password
+    {
+        public int PasswordValidation(string pswrd)
+        {
+            int res = 0;
+            var validation = pswrd.Any(char.IsDigit);
+            if (validation)
+            {
+                res = res + 1;
+            }
+
+            validation = pswrd.Any(char.IsLower);
+            if (validation)
+            {
+                res = res + 1;
+            }
+
+            validation = pswrd.Any(char.IsUpper);
+            if (validation)
+            {
+                res = res + 1;
+            }
+
+            validation = pswrd.Any(a => !char.IsLetterOrDigit(a));
+            if (validation)
+            {
+                res = res + 1;
+            }
+
+            if (pswrd.Length > 10)
+            {
+                res = res + 1;
+            }
+
+            return res;
+        }
+    }
+}
